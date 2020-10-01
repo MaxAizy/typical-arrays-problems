dee9cf3b2a76ba70ffaf3abe9cc63faf25d5ec72
ShiningLight <aizy.fpg@gmail.com>
2020-10-01 13:43:52 +0300

    feat: all cases passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    return array != undefined
+        ? array.length != 0
+            ? Math.min(...array)
+            : 0
+        : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array != undefined
+        ? array.length != 0
+            ? Math.max(...array)
+            : 0
+        : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return array != undefined
+        ? array.length != 0
+            ? array.reduce((accumulator, current) => accumulator + current) /
+              array.length
+            : 0
+        : 0;
+};
